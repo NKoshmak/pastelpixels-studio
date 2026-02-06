d6ec4684ae96ec178ab068d5de875a1b75678340
Nataliia <koshmaknatalia@gmail.com>
2026-02-06 14:20:11 -0700

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 /** @format */
-
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -9,7 +9,7 @@ export const metadata = {
   title: "Pastel Pixels Studio",
   description: "Premium templates and custom development",
   icons: {
-    icon: "/favicon.ico",      // або .ico
+    icon: "/favicon.ico", // або .ico
     shortcut: "/favicon-32x32.png",
     apple: "/apple-touch-icon.png", // якщо буде
   },
@@ -19,13 +19,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P7LH0WG3YY"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P7LH0WG3YY');
+          `}
+        </Script>
         <link rel="icon" href="/images/favicon.ico" />
       </head>
       <body>
-          <CustomCursor />
-          <Navbar />
-          {children}
-          <Footer />
+        <CustomCursor />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );

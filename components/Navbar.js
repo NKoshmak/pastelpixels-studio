a23f13e7dee819c3ad9fb7652e8b52c61da01332
Nataliia <koshmaknatalia@gmail.com>
2026-02-15 21:15:43 -0700

    adjust nav mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
+/** @format */
+
 "use client";
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,22 +14,36 @@ export default function Navbar() {
     setIsOpen(false);
   }, [pathname]);
 
-
   return (
-    <nav className="navbar">
-      <Link href="/" className="nav-left">Pastel Pixels Studio</Link>
+    <>
+      <nav className="navbar">
+        <Link href="/" className="nav-left">
+          Pastel Pixels Studio
+        </Link>
 
-      <div className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+        <div className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
 
-      <div className={`nav-center ${isOpen ? "open" : ""}`}>
-        <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
-        <Link href="/#services" onClick={() => setIsOpen(false)}>Services</Link>
-        <Link href="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
-      </div>
-    </nav>
+        <div className={`nav-center ${isOpen ? "open" : ""}`}>
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            Home
+          </Link>
+          <Link href="/#services" onClick={() => setIsOpen(false)}>
+            Services
+          </Link>
+          <Link href="/contact" onClick={() => setIsOpen(false)}>
+            Contact
+          </Link>
+        </div>
+      </nav>
+
+      <div
+        className={`nav-overlay ${isOpen ? "open" : ""}`}
+        onClick={() => setIsOpen(false)}
+      />
+    </>
   );
 }
